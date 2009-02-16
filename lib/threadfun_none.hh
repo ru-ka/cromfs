60466691bbaae6ac550af01f2049c3d5bca6a004
Joel Yliluoma <joel.yliluoma@iki.fi>
2009-02-16 13:02:11 +0200

    Fix warning in tests/run.sh

--- lib/threadfun_none.hh
+++ lib/threadfun_none.hh
@@ -27,6 +27,12 @@ public:
  */
 struct InterruptibleContext
 {
+    /* Declaring inline dummy constructor and destructor
+     * suppresses warnings about unused variable when
+     * InterruptibleContext is instantiated
+     */
+    inline InterruptibleContext() { }
+    inline ~InterruptibleContext() { }
     inline void Disable() { }
     inline void Enable() { }
 };
