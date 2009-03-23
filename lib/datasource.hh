31d8ebce5ca504e27ace0e20b8ccbf16d1069ff5
Joel Yliluoma <joel.yliluoma@iki.fi>
2009-03-23 11:31:44 +0200

    Simplify the datasource structure

--- lib/datasource.hh
+++ lib/datasource.hh
@@ -10,24 +10,14 @@
 #include <sys/stat.h>
 #include <fcntl.h> // O_RDONLY, O_LARGEFILE
 #include <string>
-#include <cstring> // std::memcpy
+#include <cstring> // std::memcpy, std::strcpy
 
 #include <vector>
 
 struct datasource_t: public ptrable
 {
-    virtual const std::vector<unsigned char> read(uint_fast64_t n) = 0;
-    virtual const std::vector<unsigned char> read(uint_fast64_t n, uint_fast64_t pos) = 0;
-    virtual void read(DataReadBuffer& buf, uint_fast64_t n)
-    {
-        std::vector<unsigned char> d = read(n);
-        buf.AssignCopyFrom(&d[0], d.size());
-    }
-    virtual void read(DataReadBuffer& buf, uint_fast64_t n, uint_fast64_t pos)
-    {
-        std::vector<unsigned char> d = read(n, pos);
-        buf.AssignCopyFrom(&d[0], d.size());
-    }
+    virtual void read(DataReadBuffer& buf, uint_fast64_t n) = 0;
+    virtual void read(DataReadBuffer& buf, uint_fast64_t n, uint_fast64_t pos) = 0;
 
     virtual bool open() { return true; }
     virtual void rewind(uint_fast64_t=0) { }
@@ -40,24 +30,18 @@ struct datasource_t: public ptrable
 struct datasource_vector: public datasource_t
 {
     datasource_vector(const std::vector<unsigned char>& vec, const std::string& nam = "")
-        : data(vec), name(nam), pos(0) { }
+        : data(vec), name( new char[ nam.size()+1 ] ), pos(0)
+    {
+        std::strcpy(name, nam.c_str());
+    }
 
+    virtual ~datasource_vector()
+    {
+        delete[] name;
+    }
 
     virtual void rewind(uint_fast64_t p) { pos = p; }
     virtual const std::string getname() const { return name; }
-    virtual const std::vector<unsigned char> read(uint_fast64_t n)
-    {
-        pos += n;
-        return std::vector<unsigned char>(
-            data.begin() + pos - n,
-            data.begin() + pos);
-    }
-    virtual const std::vector<unsigned char> read(uint_fast64_t n, uint_fast64_t p)
-    {
-        return std::vector<unsigned char>(
-            data.begin() + p,
-            data.begin() + p + n);
-    }
     virtual void read(DataReadBuffer& buf, uint_fast64_t n)
     {
         buf.AssignRefFrom(&data[pos], n);
@@ -70,31 +54,25 @@ struct datasource_vector: public datasource_t
     virtual uint_fast64_t size() const { return data.size(); }
 private:
     const std::vector<unsigned char> data;
-    std::string name;
+    char* name;
     uint_fast64_t pos;
 };
 
 struct datasource_vector_ref: public datasource_t
 {
     datasource_vector_ref(const std::vector<unsigned char>& vec, const std::string& nam = "")
-        : data(vec), name(nam), pos(0) { }
+        : data(vec), name( new char[ nam.size()+1 ] ), pos(0)
+    {
+        std::strcpy(name, nam.c_str());
+    }
 
+    virtual ~datasource_vector_ref()
+    {
+        delete[] name;
+    }
 
     virtual void rewind(uint_fast64_t p) { pos = p; }
     virtual const std::string getname() const { return name; }
-    virtual const std::vector<unsigned char> read(uint_fast64_t n)
-    {
-        pos += n;
-        return std::vector<unsigned char>(
-            data.begin() + pos - n,
-            data.begin() + pos);
-    }
-    virtual const std::vector<unsigned char> read(uint_fast64_t n, uint_fast64_t p)
-    {
-        return std::vector<unsigned char>(
-            data.begin() + p,
-            data.begin() + p + n);
-    }
     virtual void read(DataReadBuffer& buf, uint_fast64_t n)
     {
         buf.AssignRefFrom(&data[pos], n);
@@ -107,7 +85,7 @@ struct datasource_vector_ref: public datasource_t
     virtual uint_fast64_t size() const { return data.size(); }
 private:
     const std::vector<unsigned char>& data;
-    std::string name;
+    char* name;
     uint_fast64_t pos;
 };
 
@@ -117,11 +95,11 @@ private:
     enum { BufSize = 1024*256, FailSafeMMapLength = 1048576 };
 protected:
     datasource_file(int fild, uint_fast64_t s)
-        : fd(fild),siz(s), buffer(),bufpos(0),
+        : fd(fild),siz(s), pos(0),
           map_base(),map_length(),mmapping() { }
 public:
     datasource_file(int fild)
-        : fd(fild), siz(stat_get_size(fild)), buffer(),bufpos(0),
+        : fd(fild), siz(stat_get_size(fild)), pos(0),
           map_base(),map_length(),mmapping()
     {
         FadviseSequential(fd, 0, siz);
@@ -139,29 +117,26 @@ public:
     {
         if(mmapping)
             mmapping.Unmap();
-        else
-            buffer = std::vector<unsigned char> ();
-        bufpos = 0;
+        pos = 0;
     }
 
     virtual void rewind(uint_fast64_t p=0)
     {
-        bufpos=p;
+        pos=p;
         if(!mmapping)
         {
             ::lseek(fd, p, SEEK_SET);
-            buffer = std::vector<unsigned char> ();
         }
     }
     virtual const std::string getname() const { return "file"; }
     virtual void read(DataReadBuffer& buf, uint_fast64_t n)
     {
-        read(buf, n, bufpos);
-        bufpos += n;
+        read(buf, n, pos);
+        pos += n;
     }
     virtual void read(DataReadBuffer& buf, uint_fast64_t n, uint_fast64_t p)
     {
-        if(mmapping)
+        if(mmapping && n <= map_length)
         {
             if(p < map_base || p + n - map_base > map_length)
             {
@@ -171,76 +146,24 @@ public:
                 mmapping.SetMap(fd, map_base, map_length);
                 if(!mmapping) goto mmap_failed;
             }
+
+            if(p < map_base || p + n - map_base > map_length)
+            {
+                // mmapping failed to produce a region that satisfies the entire request.
+                goto mmap_failed;
+            }
+
             buf.AssignRefFrom(mmapping.get_ptr() + p - map_base, n);
         }
         else
+        {
         mmap_failed:
-            datasource_t::read(buf, n, p);
-    }
-
-    virtual const std::vector<unsigned char> read(uint_fast64_t n, uint_fast64_t p)
-    {
-        if(mmapping)
-        {
-            if(p < map_base || p + n - map_base > map_length)
+            /* Note: non-buffered reading. */
+            if(buf.LoadFrom(fd, n, p) < 0)
             {
-                mmapping.Unmap();
-                map_base   = p &~ UINT64_C(4095);
-                map_length = std::min(siz-map_base, (uint_fast64_t)FailSafeMMapLength);
-                mmapping.SetMap(fd, map_base, map_length);
-                if(!mmapping) goto mmap_failed;
-            }
-            return std::vector<unsigned char>
-                (mmapping.get_ptr() - map_base + p,
-                 mmapping.get_ptr() - map_base + p + n);
-        mmap_failed:;
-        }
-
-        std::vector<unsigned char> result(n);
-        int res = pread64(fd, &result[0], n, p);
-        if(res < 0) { std::perror(getname().c_str()); }
-        return result;
-    }
-
-    virtual const std::vector<unsigned char> read(uint_fast64_t n)
-    {
-        if(mmapping)
-        {
-            std::vector<unsigned char> retval = read(n, bufpos);
-            bufpos += n;
-            return retval;
-        }
-
-        std::vector<unsigned char> result(n);
-        uint_fast64_t result_pos    = 0;
-        uint_fast64_t result_remain = n;
-        while(result_remain > 0)
-        {
-            const size_t buf_remain = buffer.size() - bufpos;
-            if(buf_remain > 0)
-            {
-                size_t buf_consume = buf_remain;
-                if(buf_consume > result_remain) buf_consume = result_remain;
-                std::memcpy(&result[result_pos], &buffer[bufpos], buf_consume);
-                bufpos     += buf_consume;
-                result_pos += buf_consume;
-                result_remain -= buf_consume;
-            }
-            else
-            {
-                int res = ::read(fd, &buffer[0], BufSize);
-                if(res < 0) { std::perror(getname().c_str()); break; }
-                if(res == 0) break;
-                buffer.resize(res);
+                std::perror(getname().c_str());
             }
         }
-        result.resize(result_pos);
-        if(result_remain > 0)
-        {
-            fprintf(stderr, "%s: short read (%u, %u)", getname().c_str(),
-                (unsigned)result_pos, (unsigned)n);
-        }
-        return result;
     }
 
     virtual uint_fast64_t size() const { return siz; }
@@ -259,22 +182,27 @@ protected:
     }
 protected:
     int fd;
-    uint_fast64_t siz;
-    std::vector<unsigned char> buffer;
-    uint_fast64_t bufpos, map_base, map_length;
+    uint_fast64_t siz, pos, map_base, map_length;
     MemMappingType<true> mmapping;
 };
 
 struct datasource_file_name: public datasource_file
 {
-    datasource_file_name(const std::string& p):
-        datasource_file(-1, stat_get_size(p)), path(p)
+    datasource_file_name(const std::string& nam):
+        datasource_file(-1, stat_get_size(nam)), path( new char [nam.size()+1] )
     {
+        std::strcpy(path, nam.c_str());
     }
+    virtual ~datasource_file_name()
+    {
+        close();
+        delete[] path;
+    }
+
     virtual bool open()
     {
-        fd = ::open(path.c_str(), O_RDONLY | O_LARGEFILE);
-        if(fd < 0) { std::perror(path.c_str()); return false; }
+        fd = ::open(path, O_RDONLY | O_LARGEFILE);
+        if(fd < 0) { std::perror(path); return false; }
         FadviseSequential(fd, 0, siz);
         FadviseNoReuse(fd, 0, siz);
         return datasource_file::open();
@@ -284,13 +212,9 @@ struct datasource_file_name: public datasource_file
         datasource_file::close();
         if(fd >= 0) { ::close(fd); fd = -1; }
     }
-    virtual ~datasource_file_name()
-    {
-        close();
-    }
     virtual const std::string getname() const { return path; }
 private:
-    std::string path;
+    char* path;
 };
 
 #endif
